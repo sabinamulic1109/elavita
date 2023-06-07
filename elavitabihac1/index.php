3a88370eaa0ca5bd35524cb9d6006f7d0ce6b22b
sabinamulic0604 <sabina@stagecoding.com>
2023-06-07 18:38:08 +0200

    Second

---
+++ elavitabihac1/index.php
@@ -0,0 +1,486 @@
+<?php
+
+$conn = mysqli_connect('db02.hosting.bhtelecom.ba','c1203elavita','elmalejlaamra','c1203_form') or die('connection failed');
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $number = $_POST['number'];
+   $poruka = $_POST['poruka'];
+
+   $insert = mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, poruka) VALUES('$name','$email','$number','$poruka')") or die('query failed');
+
+   if($insert){
+      $message[] = 'Poruka uspješno poslana!';
+   }else{
+      $message[] = 'Greška.. Molimo pokušajte ponovno.';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ELA VITA</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- bootstrap cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<header class="header fixed-top">
+
+   <div class="container">
+
+
+      <div class="row align-items-center justify-content-between">
+
+         <a href="#home" class="logo"><img class="logo1" src="images/elavita.jpg">ELA<span>VITA</span></a>
+
+         <nav class="nav">
+            <a href="#home">Početna</a>
+            <a href="#about">O nama</a>
+            <a href="#services">Naše usluge</a>
+            <a href="#cjenovnik">Cjenovnik</a>
+            <a href="#dokumenti">Dokumentacija</a>
+            <a href="#contact">Kontakt</a>
+         </nav>
+
+         <a href="#contact" class="link-btn">KONTAKTIRAJTE NAS!</a>
+
+         <div id="menu-btn" class="fas fa-bars"></div>
+
+      </div>
+
+   </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+   <div class="container">
+
+      <div class="row min-vh-100 align-items-center">
+        
+         <div class="content text-center text-md-left">
+            <p>
+               <h1>AGENCIJA ZA NJEGU I BRIGU U KUĆI</h1>
+
+<img src="images/pocetno.png" class="slika">
+            </p>
+            <h3><i>Njega s povjerenjem</i></h3><br>
+
+               <hr>
+               <p class="box" style="color:#ad2152; font-size:23px;"> 
+                  <i class="fas fa-map-marker-alt" ></i> Plješavička 67, Bihać<br>
+               <i class="fas fa-phone"></i> +387 62 977 180<br>
+            <i class="fas fa-envelope"></i> elavitabihac@gmail.com</a><br><hr>
+          <br>  <a href="#contact" class="link-btn">POSTANITE NAŠ KLIJENT</a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+   <div class="container">
+
+      <div class="row align-items-center">
+
+         <div class="col-md-6 image">
+            <img src="images/onama.jpg" class="w-100 mb-5 mb-md-0" alt="">
+         </div>
+
+         <div class="col-md-6 content">
+            <span>O NAMA</span>
+            <h3>Briga je stvar povjerenja</h3>
+            <p style="text-align:justify; text-transform: none;">Za zdravlje i dobrobit pojedinca od ključne je važnosti kvaliteta života. Značajan porast udjela starijeg stanovništva, kao jedne od socijalno ranjivih skupina, u ukupnom stanovništvu, povećava potrebe za socijalnom pomoć te socijalnim uslugama o kojima ovisi kvaliteta života starijih osoba.</p>
+            <p style="text-align:justify; text-transform: none;">Osjećaj zadovoljstva i ispunjenosti starijim osobama jednako je važan za opću dobrobit kao i redoviti pregledi kod ljekara. Zapravo, pozitivan pogled na život može im pomoći da imaju više energije, manje stresa i bolji apetit. 
+               Korisnicima naših usluga možemo osigurati pomoć i njegu u kući za zadovoljavanje pojedinih potreba koje mu, zbog današnjeg savremenog načina života, ne mogu pružiti članovi njegove porodice.Srodna djelatnost ELA VITA nudi vam tehnički kompetentnu skrb i podršku u vaša četiri zida. </p>
+               <p style="text-align:justify; text-transform: none;">Pružamo pomoć kad god vam je potrebna i uvijek aktivno slušamo vaše probleme. Na taj način pomažemo vam da živite u velikoj mjeri neovisno u svom domu.
+               </p>
+            <a href="#services" class="link-btn">Pregled usluga</a>
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+   <h1 class="heading">usluge koje nudimo</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/icon-1.JPG" alt="">
+         <h3>POMOĆ PRI ZADOVOLJAVANJU OSNOVNIH ŽIVOTNIH POTREBA</h3>
+ <p style="text-align:justify; text-transform:none;">
+   Uključuje usluge na dnevnoj bazi kao što su kupanje, presvlačenje, pomoć pri kretanju, pomoć pri hranjenju, nabavka namirnica, nabavka lijekova.           
+            </p>
+      </div>
+      <div class="box">
+         <img src="images/temperatura.jpg" alt="">
+         <h3>ZDRAVSTVENA NJEGA</h3>
+         <p style="text-align:justify; text-transform:none;">
+            Kontrola uzimanja lijekova i kontrola prehrane klijenta. Mjerenje i praćenje visine pritiska, šećera i masnoće, tjelesne temperature, davanje injekcija, previjanje rana, te ostale potrebne radnje u vidu ispunjenja zdravstene njege. </p>      </div>
+      <div class="box">
+         <img src="images/H.jpg" alt="">
+         <h3>DRUŽENJE I RAZGOVOR</h3>
+         <p style="text-align:justify; text-transform:none;">
+            Garantujemo ljubaznost i posvećenost u trenutcima druženja s klijentom, te razvijanje povjerenja između osoblja i klijenta. društveni razgovori i poticanje govorenja vrlo su važni za spriječavanje depresije i osjećaja usamljenosti.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/srce.jpg" alt="">
+         <h3>PRUŽANJE OSJEĆAJA ZAŠTITE I SIGURNOSTI</h3>
+         <p style="text-align:justify; text-transform:none;">Pružanje apsolutne zaštite i potpore našim klijentima. Provođenje vremena s njima, šetnje ili igranje društvenih igara, tako da znaju da smo uvijek tu za njih ne samo poslovno, nego i kao prijatelji.</p>
+      </div>
+
+      
+
+      <div class="box">
+         <img src="images/slusanje.jpg" alt="">
+         <h3 >OBAVJEŠTAVANJE ČLANOVA PORODICE O STANJU</h3>
+         <p style="text-align:justify; text-transform:none;">U mogućnosti smo kontinuirano obavještavati članove porodice o pruženim uslugama, te zdravstvenom i mentalnom stanju klijenta putem telefonske ili video komunikacije s vašim članom porodice u prisustvu našeg osoblja.</p>      </div>
+
+      <div class="box">
+         <img src="images/lijekovi.jpg" alt="">
+         <h3>OBAVLJANJE DRUGIH ADMINISTRATIVNIH POSLOVA</h3>
+         <p style="text-align:justify; text-transform:none;">Svi drugi potrebni poslovi koje klijenti nisu u stanju ili mogućnosti odraditi, poput odlaska u banku, poštu, obavljanje kupovine, podizanje recepata, odlazak u ljekarnu i podizanje lijekova, rezervacija termina kod doktora, itd..</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- process section starts  -->
+
+<section class="process">
+
+   <h1 class="heading">Angažiranje kućne pomoći</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/treca_b.png" alt="">
+         <h3>Probni rad</h3>
+         <p>Upoznajte osoblje i njihov način rada. Uvjerite se u našu profesionalnost.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/druga.png" alt="">
+         <h3>Definirajte aktivnosti</h3>
+         <p>Unaprijed tačno koordinirajte koje aktivnosti treba provesti, kako i kada. </p>
+      </div>
+
+      <div class="box">
+         <img src="images/treca_a.png" alt="">
+         <h3>Izgradite povjerenje</h3>
+         <p>U svakom trenutku su dobrodošle provjere napretka rada i stanja klijenta.</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- process section ends -->
+
+<!--cjenovnik-->
+
+
+<section class="cjenovnik" id="cjenovnik">
+
+
+   <h1 class="heading">pregled cijena</h1>
+
+   <table class="table table-bordered " style="font-size:17px;  color: antiquewhite; background-color: #ad2152; ">
+  <thead>
+   <tr>
+      
+      <th scope="col" colspan="2" style="text-align: center;">Ponuda usluga</th>
+      <th scope="col" style="text-align: center;">Cijena </th>
+    </tr>
+  </thead>
+
+  <tr>
+      
+      <td colspan="2">DNEVNA POMOĆ U KUĆI <hr>
+     <p style="font-size: 1.2rem; text-align: justify; font-weight: normal;"> Čišćenje, kupovina namirnica, plaćanje računa, vođenje liječniku, itd..
+   </p>
+   </td>
+      <td>10.00 KM po satu
+   </td>
+    </tr>
+   <tr>
+      
+      <td colspan="2">INICIJALNA PROCJENA <hr>
+   
+         <p style="font-size: 1.2rem; text-align: justify; font-weight: normal;">   Inicijalna procjena podrazumijeva upoznavanje 
+      tima ELA VITA sa korisnikom usluge. 
+      Upoznajemo se sa socijalnom situacijom pacijenta, 
+      njegovim zdravstvenim stanjem kroz ocjenu 
+      kategorizacije obzirom na stepen samostalnosti i 
+      potrebama. Također pacijent treba da iskaže svoje 
+      lične potrebe i očekivanja od tima ELA VITA.</p>
+   </td>
+      <td>30.00 KM
+   </td>
+    </tr>
+
+    
+   <tr>
+      
+      <td colspan="2">IZRADA PLANA BRIGE<hr>
+         <p style="font-size: 1.2rem; text-align: justify; font-weight: normal;">
+   Plan brige nastaje na osnovu  trenutnih tegoba korisinika 
+   usluge, sa smanjenom mogućnošću brige o sebi, kroz akcije 
+   kratkoročnih i dugoročnih potreba koje obuhvataju 
+   intervencije, edukaciju zatim mjere koje će odložiti ili 
+   izostaviti, nastanak kasnih komplikacija u cilju 
+   poboljšanja kvalitete života.</p> </td>
+      <td>50.00 KM 
+   </td>
+    </tr>
+
+    
+   <tr>
+      
+      <td colspan="2">POSJETA KOORDINATORA BRIGE / 1h <hr>
+         <p style="font-size: 1.2rem; text-align: justify; font-weight: normal;">   Tim ELA VITA pruža usluge jutarnje i/ili večernje 
+      higijenske toalete koje podrazumijevaju: 
+      umivanje, promjenu pelena, pranje genitalnog područja, 
+      mazanje kreme, mjerenje pritiska i davanje terapije u 
+      obliku tableta. Također, podrazumijeva sve potrebne 
+      radnje iskazane od korisnika kao što su čišćenje, 
+      kupovina namirnica, plaćanje računa, vođenje liječniku 
+      u trajanju od 1 sata.</p>
+   
+   </td>
+      <td>17.00 KM</td>
+    </tr>
+
+    
+   <tr>
+      
+      <td colspan="2"><b>POSJETA KOORDINATORA BRIGE / 2h</b><hr>
+   
+         <p style="font-size: 1.2rem; text-align: justify; font-weight: normal;">   Kod polupokretnih ili nepokretnih bolesnika određivanje 
+      sedmičnog plana za kupanje bolesnika u krevetu ili 
+      odvođenje u kupatilo, pranje kose, siječenje noktiju, 
+      mijenjanje posteljine i sl. Svakodnevna briga o cijelom 
+      tijelu starih i bolesnih ljudi je važna da bi se 
+      spriječio dekubitus (kod dužeg ležanja), i da bi se 
+      bolesnik bolje osjećao
+Podrazumijeva sve potrebne radnje iskazane od korisnika 
+čišćenje, kupovina namirnica, plaćanje računa, vođenje 
+liječniku… u trajanju od 2 sata. </p>
+
+   </td>
+      <td><b>34.00 KM</b></td>
+    </tr>
+
+    
+
+    <tr>
+      
+      <td colspan="2"><b>TELEFONSKE KONSULTACIJE</b> <hr> <p style="font-size: 1.2rem; text-align: justify; font-weight: normal;">
+      Korisnicima naših usluga nudimo opciju da putem 
+      telefona, e-maila potraže savjet o brzi i njezi 
+      članova svoje porodice.</p></td>
+      <td><b>10.00 KM</b></td>
+    </tr>
+
+   
+    <tr>
+      
+      <td colspan="2">SAVJETOVANJE / RAZGOVOR <hr>
+   
+         <p style="font-size: 1.2rem; text-align: justify; font-weight: normal;"> Radna terapija u cilju očuvanja kognitivne i tjelesne 
+      sposobnosti (aktivno sudjelovanje u životu). Upotreba 
+      različitih stručnjaka uz rutinsko praćenje za 
+      evaluaciju, dijagnostičko praćenje uzroka i liječenja.</p> 
+   </td>
+      <td>50.00 KM</td>
+    </tr>
+    <tr>
+      
+      <td colspan="2">EDUKACIJA</td>
+      <td>50.00 KM</td>
+    </tr>
+
+    <tr>
+      
+      <td colspan="2">IZDAVANJE MIŠLJENJA O STANJU KLIJENTA</td>
+      <td>25.00 KM</td>
+    </tr>
+
+    
+    <tr >
+      <td colspan="3" style="text-align: center;">Mogućnost dogovora kombinacijom usluga i cijena.</td>
+    </tr>
+
+
+
+  <tbody>
+    
+</table>
+
+</section>
+
+
+
+<!-- reviews section starts  -->
+
+<section class="reviews" id="dokumenti">
+
+   <h1 class="heading"> dodatne informacije </h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/elavita.jpg" alt="">
+         <p style="color: black; text-align:justify;">Za usluge kućne njege potrebno je da se "ELA VITA" centru obrati lično ili neko od srodnika.
+            Zahtjev se podnosi u pisanoj formi i može se preuzeti u download zoni web sajta. 
+            Ispunjen zahtjev predajete u prostorije centra "ELA VITA" na adresi Plješavička 67, Bihać. 
+            U kontakt formi možete naći dodate informacije na koji način možete stupiti s nama u kontakt, te je moguće uz dogovor napraviti kombinaciju usluga i cijena po Vašoj želji.
+           
+            </p>
+      
+         
+      </div>
+
+      
+
+      <div class="box1">
+         <h3>Download zona</h3>
+         <span>Ovdje možete preuzeti potrebnu dokumentaciju.</span>
+         <p><a href="https://drive.google.com/file/d/1y3IKKeZl7aONdyKlNrzMalc2mcxYvrCI/view?usp=sharing" download><i class="fas fa-download"></i> POTREBNA DOKUMENTACIJA</a> </p>
+         <p><a href="https://drive.google.com/file/d/18CmgV4OYP7wi_yfZN6WQm1cYUzmTQC3H/view?usp=sharing" download><i class="fas fa-download"></i> ZAHTJEV ZA USLUGE</a> </p>
+         <p><a href="https://drive.google.com/file/d/18FL5I_BSw7E23R0cy5Ec9Ldu0SxvfNI2/view?usp=sharing" download><i class="fas fa-download"></i> CJENOVNIK</a> </p>
+         <p><a href="https://drive.google.com/file/d/1Jun6hhkCrk9Ez_MDWomM0UiMZvQlte5C/view?usp=sharing" download><i class="fas fa-download"></i> ELA VITA</a> </p>
+
+        
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+   <h1 class="heading">kontakt forma</h1>
+
+   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      
+   <?php 
+         if(isset($message)){
+            foreach($message as $message){
+               echo '<p class="obavijest">'.$message.'</p>';
+            }
+         }
+      ?>
+      <span>Unesite Vaše ime i prezime :</span>
+      <input type="text" name="name" placeholder="" class="box" required>
+      <span>Unesite Vaš mail :</span>
+      <input type="email" name="email" placeholder="" class="box" required>
+      <span>Unesite broj mobitela :</span>
+      <input type="number" name="number" placeholder="" class="box" required>
+      <span>Unesite Vašu poruku :</span>
+      <textarea class="box" name="poruka"></textarea>
+      <input type="submit" value="POŠALJI" name="submit" class="link-btn" >
+      </form>  
+ 
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container container">
+
+      <div class="box">
+         <i class="fas fa-phone"></i>
+         <h3>Broj telefona</h3>
+         <p>+387 62 977 180 </p>
+      </div>
+      
+      <div class="box">
+         <i class="fas fa-map-marker-alt"></i>
+         <h3>Adresa</h3>
+         <p>Plješavička 67, 77000 Bihać, BiH</p>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-clock"></i>
+         <h3>Radno vrijme</h3>
+         <p>PON-SUB 07-15:30h</p>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-envelope"></i>
+         <h3>Adresa e-pošte</h3>
+         <p>elavitabihac@gmail.com</p>
+      </div>
+
+   </div>
+
+   <div class="credit"> &copy; Copyright <?php echo date('Y'); ?> by <span>S. Mulić</span>  </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
